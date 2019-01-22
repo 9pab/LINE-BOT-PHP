2fb5cb64df35ff0c84e1712b0f687c606087b6bb
9pab <32660521+9pab@users.noreply.github.com>
2019-01-22 13:33:40 +0700

    Create sms.php

---
+++ sms.php
@@ -0,0 +1,11 @@
+<?php
+require "vendor/autoload.php";
+$access_token = 'Bv/PUIP/rMX3jysSVM2dP1KVAZDJPIa2MLngjgcXFoZ35bH/h1vJiQdx0ZIrhsNqXR+XwnlDxxU1R9SSbKVSQFbKj03ZGEnRmakhwQw7qbSzyOqMkzrRuK9rH4/t82AY8ukObORKpvQCdLTXVtJWzQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'd004cf371d24fc6b9a6941c8b1103860';
+$pushID = 'U3892909119edd655235f467088f954f6';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($sms);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+?>
