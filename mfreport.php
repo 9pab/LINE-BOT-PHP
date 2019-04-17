1732c11c3dbf8efeb64f08a0f679313e1e2783b4
9pab <32660521+9pab@users.noreply.github.com>
2019-04-17 12:50:18 +0700

    Update mfreport.php

--- mfreport.php
+++ mfreport.php
@@ -84,6 +84,7 @@ print '<script>
     html2canvas(document.body).then(function(canvas) {
     // Export the canvas to its data URI representation
     var base64image = canvas.toDataURL("image/png");
+    document.body.appendChild(canvas);
 
     // Open the image in a new window
     window.open(base64image , "_blank");
