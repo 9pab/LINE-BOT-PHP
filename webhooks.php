e7fd3c5c58b4ee36c8e4ff0cbf3f08cbfe465d7c
9pab <32660521+9pab@users.noreply.github.com>
2019-08-28 09:29:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "สวัสดี\n".$text."\nreplyToken\n".$replyToken
+				'text' => "สวัสดี\n".$text."\nreplyToken\n".$replyToken."\nraw\n".$events
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
