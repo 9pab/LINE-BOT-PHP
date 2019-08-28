a49d1f375c3da228e774afb40af93ca30f801f72
9pab <32660521+9pab@users.noreply.github.com>
2019-08-28 11:35:28 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "สวัสดี\n".$UID."\n"."replyToken\n".$replyToken
+				'text' => "Hello\n".$UID."\n"."replyToken\n".$replyToken
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
