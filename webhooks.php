45bf4ce8a57fd4203ce3b39d527424f2078b08be
9pab <piboonsak@gmail.com>
2019-09-25 14:16:59 +0700

    25 Sep

--- webhooks.php
+++ webhooks.php
@@ -45,7 +45,7 @@ if (!is_null($events['events'])) {
 					break;
 				default :
 					$arrayPostData['messages'][0]['type'] = "text";
-					$arrayPostData['messages'][0]['text'] = "กด 1 ดูพอร์ตการลงทุน\nกด 2 แสดง content";
+					$arrayPostData['messages'][0]['text'] = "กด 1 ดูพอร์ตการลงทุน\nกด 2 แสดง content\nกด 3 แสดง sticker";
 			}
 		} else {
 			// Build message to reply back when is not 'text' format
