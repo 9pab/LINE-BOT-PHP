41a34662a613228d6abd81720c73e303a5034ea1
9pab <piboonsak@gmail.com>
2019-11-06 16:18:01 +0700

    Flex message

--- push.php
+++ push.php
@@ -38,33 +38,29 @@ $arrayPostData = array(
 	'to' => $pushID,
 	'messages' => array(
 		0 => array(
-			"type" => "text",
-			"text" => "Test flex message",
+			'type' => 'text',
+			'text' => 'Test flex message',
 		),
-		1 => array (
+		1 => array(
 			'type' => 'flex',
 			'altText' => 'this is a flex message',
-			'contents' => 
-			array (
-			  'type' => 'bubble',
-			  'body' => 
-			  array (
-				'type' => 'box',
-				'layout' => 'vertical',
-				'contents' => 
-				array (
-				  0 => 
-				  array (
-					'type' => 'text',
-					'text' => 'hello',
-				  ),
-				  1 => 
-				  array (
-					'type' => 'text',
-					'text' => 'world',
-				  ),
+			'contents' => array(
+				'type' => 'bubble',
+				'size' => 'giga',
+				'body' => array(
+					'type' => 'box',
+					'layout' => 'vertical',
+					'contents' => array(
+						0 => array(
+							'type' => 'text',
+							'text' => 'hello',
+						),
+						1 => array(
+							'type' => 'text',
+							'text' => 'world',
+						),
+					),
 				),
-			  ),
 			),
 		),
 	),
