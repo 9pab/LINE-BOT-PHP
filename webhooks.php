7a9f8bc4dc1225539e4cf3f56c3164f6dfa29898
9pab <piboonsak@gmail.com>
2019-11-06 16:34:07 +0700

    Giga size

--- webhooks.php
+++ webhooks.php
@@ -47,6 +47,7 @@ if (!is_null($events['events'])) {
 					$arrayPostData['messages'][1]['type'] = "flex";
 					$arrayPostData['messages'][1]['altText'] = "Fund Portfolio";
 					$arrayPostData['messages'][1]['contents']['type'] = "bubble";
+					$arrayPostData['messages'][1]['contents']['size'] = "giga";
 					$arrayPostData['messages'][1]['contents']['body']['type'] = "box";
 					$arrayPostData['messages'][1]['contents']['body']['layout'] = "vertical";
 					$arrayPostData['messages'][1]['contents']['body']['contents'][0]['type'] = "text";
@@ -56,7 +57,7 @@ if (!is_null($events['events'])) {
 				break;
 				default :
 					$arrayPostData['messages'][0]['type'] = "text";
-					$arrayPostData['messages'][0]['text'] = "กด 1 ดูพอร์ตการลงทุน\nกด 2 แสดง content\nกด 3 Flex message 16:31";
+					$arrayPostData['messages'][0]['text'] = "กด 1 ดูพอร์ตการลงทุน\nกด 2 แสดง content\nกด 3 Flex message giga";
 			}
 		} else {
 			// Build message to reply back when is not 'text' format
