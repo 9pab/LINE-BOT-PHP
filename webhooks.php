7bea30325993f8f46bc107b0d766e71cfcb49908
9pab <piboonsak@gmail.com>
2019-11-06 12:37:13 +0700

    flex

--- webhooks.php
+++ webhooks.php
@@ -42,7 +42,7 @@ if (!is_null($events['events'])) {
 					$arrayPostData['messages'][0]['type'] = "flex";
 					$arrayPostData['messages'][0]['altText'] = "Fund Portfolio";
 					$arrayPostData['messages'][0]['contents']['type'] = "bouble:";
-					$arrayPostData['messages'][0]['contents']['size'] = "giga";
+					//$arrayPostData['messages'][0]['contents']['size'] = "giga";
 					$arrayPostData['messages'][0]['contents']['body']['type'] = "box";
 					$arrayPostData['messages'][0]['contents']['body']['contents'][0]['type'] = "text";
 					$arrayPostData['messages'][0]['contents']['body']['contents'][0]['text'] = "Fund Portfolio";
