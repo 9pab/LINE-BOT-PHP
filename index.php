a37871bcafa9b2b09ceaa1bef178a39b7f5de60d
9pab <piboonsak@gmail.com>
2019-11-07 13:17:41 +0700

    test

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-require "vendor/autoload.php";
-$access_token = 'Bv/PUIP/rMX3jysSVM2dP1KVAZDJPIa2MLngjgcXFoZ35bH/h1vJiQdx0ZIrhsNqXR+XwnlDxxU1R9SSbKVSQFbKj03ZGEnRmakhwQw7qbSzyOqMkzrRuK9rH4/t82AY8ukObORKpvQCdLTXVtJWzQdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'd004cf371d24fc6b9a6941c8b1103860';
-$idPush = 'U3892909119edd655235f467088f954f6'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+phpinfo();
+?>
