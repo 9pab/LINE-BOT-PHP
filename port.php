44fcd2dc5324860768cecd5cf3d75314deef9e1c
9pab <piboonsak@gmail.com>
2019-11-07 15:15:44 +0700

    lastval

--- port.php
+++ port.php
@@ -102,7 +102,7 @@ foreach ($port as $i => $v) {
     curl_close($ch);
 
     $data = json_decode($result);
-    print $i . " = " . $v['name'] . "   NAV = ". $data['last_val'] . "<br>";
+    print $i . " = " . $v['name'] . "   NAV = ". $data->{'last_val'} . "<br>";
 }
 
 //Show result
