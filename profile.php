aba122f78f5c2ae3766fe09331ef6bff73b3b34e
9pab <piboonsak@gmail.com>
2019-11-11 16:23:52 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+//$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+//$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
-
-//$userId = 'U3892909119edd655235f467088f954f6';
+$access_token = 'Bv/PUIP/rMX3jysSVM2dP1KVAZDJPIa2MLngjgcXFoZ35bH/h1vJiQdx0ZIrhsNqXR+XwnlDxxU1R9SSbKVSQFbKj03ZGEnRmakhwQw7qbSzyOqMkzrRuK9rH4/t82AY8ukObORKpvQCdLTXVtJWzQdB04t89/1O/w1cDnyilFU=';
+$userId = 'U3892909119edd655235f467088f954f6';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
