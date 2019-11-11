ec2891f787a7bf7bc7ef489fac0f9bcd05a0d33d
9pab <piboonsak@gmail.com>
2019-11-11 08:50:20 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -43,7 +43,7 @@ if (!is_null($events['events'])) {
 				case "3" :
 					$arrayPostData['messages'][0]['type'] = "text";
 					$arrayPostData['messages'][0]['text'] = "Flex";
-
+/*
 					$arrayPostData['messages'][1]['type'] = "flex";
 					$arrayPostData['messages'][1]['altText'] = "Fund Portfolio";
 					$arrayPostData['messages'][1]['contents']['type'] = "bubble";
@@ -54,9 +54,9 @@ if (!is_null($events['events'])) {
 					$arrayPostData['messages'][1]['contents']['body']['contents'][0]['text'] = "Hello 1";
 					$arrayPostData['messages'][1]['contents']['body']['contents'][1]['type'] = "text";
 					$arrayPostData['messages'][1]['contents']['body']['contents'][1]['text'] = "Hello 2";
-
-					//$txt = '{"type": "flex","altText": "Flex Message","contents": {"type": "bubble","size": "giga","body": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "World 1"},{"type": "text","text": "World 2"}]}}}';
-					//array_push($arrayPostData['messages'],json_decode($txt, TRUE));
+*/
+					$txt = '{"type": "flex","altText": "Flex Message","contents": {"type": "bubble","size": "giga","body": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "World 1"},{"type": "text","text": "World 2"}]}}}';
+					array_push($arrayPostData['messages'],json_decode($txt, TRUE));
 				break;
 				default :
 					$arrayPostData['messages'][0]['type'] = "text";
