e967a6f7800cf7d97b413bcc362d5454223692ef
9pab <piboonsak@gmail.com>
2019-11-11 08:48:31 +0700

    push

--- webhooks.php
+++ webhooks.php
@@ -55,8 +55,8 @@ if (!is_null($events['events'])) {
 					$arrayPostData['messages'][1]['contents']['body']['contents'][1]['type'] = "text";
 					$arrayPostData['messages'][1]['contents']['body']['contents'][1]['text'] = "Hello 2";
 
-					$txt = '{"type": "flex","altText": "Flex Message","contents": {"type": "bubble","size": "giga","body": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "World 1"},{"type": "text","text": "World 2"}]}}}';
-					array_push($arrayPostData['messages'],json_decode($txt, TRUE));
+					//$txt = '{"type": "flex","altText": "Flex Message","contents": {"type": "bubble","size": "giga","body": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "World 1"},{"type": "text","text": "World 2"}]}}}';
+					//array_push($arrayPostData['messages'],json_decode($txt, TRUE));
 				break;
 				default :
 					$arrayPostData['messages'][0]['type'] = "text";
