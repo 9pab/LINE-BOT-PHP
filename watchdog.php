dbf1702caebac4cbbc2f8f2f6d36b0f1c2fcf8f9
9pab <piboonsak@gmail.com>
2020-02-20 11:24:34 +0700

    ีupdate

--- watchdog.php
+++ watchdog.php
@@ -24,12 +24,12 @@ if (!is_null($events['events'])) {
 			
 			switch ($command) {
 				case "open" :
-					//$ch = curl_init('https://maker.ifttt.com/trigger/GarageON0/with/key/bLkwIlTssz5qlFsS56rgws');
-					//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-					//curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-					//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_string)));
-					//$result = curl_exec($ch);
+					$url = 'https://maker.ifttt.com/trigger/GarageON0/with/key/bLkwIlTssz5qlFsS56rgws';
+					$handle = curl_init($url);
+					curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+					$resp = curl_exec($handle);
+					curl_close($handle);
+
 					$arrayPostData['messages'][0]['type'] = "text";
 					$arrayPostData['messages'][0]['text'] = "เปิดไฟแล้วจ้า 💡";
 					$arrayPostData['messages'][1]['type'] = "sticker";
